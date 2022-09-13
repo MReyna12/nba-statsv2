e6496dfd44daa212879a2638e1b9d78ab2828f57
Michael <mreyna12@live.com>
2022-09-13 16:02:56 -0500

    rate limiter

--- helpers/avoid-raite-limit.js
+++ helpers/avoid-raite-limit.js
@@ -1,5 +1,3 @@
-// NextJS build makes too many calls to the API when building for production, which causes the API to return an error.
-// This function allows the build process to go through.
 export async function avoidRateLimit() {
   if (process.env.NODE_ENV === "production") {
     await sleep();
